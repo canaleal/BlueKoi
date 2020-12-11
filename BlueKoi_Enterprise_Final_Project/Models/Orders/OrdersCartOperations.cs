88729964b111b0dc5ef169b0f07cc3db2b62fa2a
Alex Canales <canaleal@sheridanc.on.ca>
2020-12-11 17:48:15 -0500

    Commets for orders

--- BlueKoi_Enterprise_Final_Project/Models/Orders/OrdersCartOperations.cs
+++ BlueKoi_Enterprise_Final_Project/Models/Orders/OrdersCartOperations.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 //Alex
 namespace BlueKoi_Enterprise_Final_Project.Models.Orders
 {
+    /// <summary>
+    /// Orders Cart operations class to get, update, add, and delete an order cart and orders from the database
+    /// </summary>
     public class OrdersCartOperations : IOrdersCartRepository
     {
         private readonly VirtualStoreDBContext context;
@@ -15,24 +18,42 @@ namespace BlueKoi_Enterprise_Final_Project.Models.Orders
             this.context = context;
         }
 
+        /// <summary>
+        /// Get an accounts order cart from the database
+        /// </summary>
+        /// <param name="accountId">The account id used to compare to the account id foreign key in the ordercart table</param>
+        /// <returns>The order cart of the particular user</returns>
         public OrdersCart GetAnOrdersCart(int accountId)
         {
             OrdersCart ordersCart = context.OrdersCarts.Where(a => a.AccountId == accountId).FirstOrDefault();
             return ordersCart;
         }
 
+        /// <summary>
+        /// Add an orders cart to the database
+        /// </summary>
+        /// <param name="ordersCart">The orders cart that will be saved in the database</param>
         public void Add(OrdersCart ordersCart)
         {
             context.OrdersCarts.Add(ordersCart);
             context.SaveChanges();
         }
 
+        /// <summary>
+        /// Delete an orders cart from the database using the account id
+        /// </summary>
+        /// <param name="accountId">Account id used to compare to the foreign key in orderscart table</param>
         public void Delete(int accountId)
         {
             context.OrdersCarts.Remove(context.OrdersCarts.Single(a => a.AccountId == accountId));
             context.SaveChanges();
         }
 
+        /// <summary>
+        /// Get all orders using the orders cart id to compare
+        /// </summary>
+        /// <param name="orderCartId">The orders cart id used to compare to the foreign key in orders</param>
+        /// <returns></returns>
         public IEnumerable<Order> GetOrders(int orderCartId)
         {
             var data = from st in context.Orders
@@ -43,12 +64,20 @@ namespace BlueKoi_Enterprise_Final_Project.Models.Orders
 
         }
 
+        /// <summary>
+        /// Add a new order to the database
+        /// </summary>
+        /// <param name="order">The order that will be saved into the database</param>
         public void AddOrder(Order order)
         {
             context.Orders.Add(order);
             context.SaveChanges();
         }
 
+        /// <summary>
+        /// Delete all orders in the database that match the order cart id
+        /// </summary>
+        /// <param name="orderCartId">The order cart id used to compare to the orders foreign key</param>
         public void DeleteOrders(int orderCartId)
         {
             context.Orders.RemoveRange(context.Orders.Where(x => x.OrderCartId == orderCartId));
